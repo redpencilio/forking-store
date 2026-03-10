cbe16b8176942b074706835da3bc6e82d03007af
Elena Poelman <elena.poelman@redpencil.io>
2026-03-10 15:05:44 +0100

    woodpecker: adjust lint step

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 // rollup.config.js
 import typescript from "@rollup/plugin-typescript";
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/forking-store.ts",
